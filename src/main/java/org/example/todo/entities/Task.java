26bc2e003f81d5adda003811fe8a96a5e527f0d4
Maverick <maverickspb78@gmail.com>
2022-03-07 02:10:06 +0300

    Task8

--- src/main/java/org/example/todo/entities/Task.java
+++ src/main/java/org/example/todo/entities/Task.java
@@ -1,23 +1,28 @@
 package org.example.todo.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+@AllArgsConstructor
 @Data
+@NoArgsConstructor
+@Table(name = "task")
+@Entity
 public class Task {
 
     @NotBlank
     @NotNull
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private boolean done;
     @NotBlank
     @NotNull
     private String description;
 
-    public Task(Long id, String description) {
-        this.id = id;
-        this.description = description;
-    }
 }
---
+++ src/main/java/org/example/todo/repositories/TaskRepository.java
@@ -0,0 +1,9 @@
+package org.example.todo.repositories;
+
+import org.example.todo.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
